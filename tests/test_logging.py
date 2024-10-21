85e04e2a6ea1f5f165747dbb6bbe99eae8555a7f
Daniel Tchoń <dantch@onet.eu>
2024-10-21 23:01:21 +0200

    :white_check_mark: Fix logging tests where closing file in temp dir removes it

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -14,11 +14,10 @@ class TestLogging(unittest.TestCase):
         self.routine = Routine.from_string(get_yaml('test_ferrocene.yaml'))
 
     def test_file_handler(self) -> None:
-        with tempfile.TemporaryDirectory() as temp_dir:
+        with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as temp_dir:
             log_path = Path(temp_dir) / 'picometer.log'
-            handler = add_file_handler(log_path)
+            _ = add_file_handler(log_path)
             _ = process(self.routine)
-            handler.close()
             with open(log_path, 'r') as log_file:
                 lines = log_file.readlines()
                 self.assertGreater(len(lines), 1600)
@@ -26,11 +25,10 @@ class TestLogging(unittest.TestCase):
     def test_log_listener(self) -> None:
         snatched_log_msg = []
         snatch_log_msg = lambda s: snatched_log_msg.append(s)
-        with tempfile.TemporaryDirectory() as temp_dir:
+        with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as temp_dir:
             log_path = Path(temp_dir) / 'picometer.log'
-            handler = add_file_handler(log_path)
+            _ = add_file_handler(log_path)
             register_log_listener(snatch_log_msg)
             _ = process(self.routine)
-            handler.close()
             with open(log_path, 'r') as log_file:
                 self.assertEqual(snatched_log_msg, log_file.read().splitlines())
